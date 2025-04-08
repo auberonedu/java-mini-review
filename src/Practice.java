2db7ce020241e05da4264df96a08691ff9f3b821
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-04-08 09:42:48 -0700

    The method for printItems is completed

--- src/Practice.java
+++ src/Practice.java
@@ -18,6 +18,9 @@ public class Practice {
      */
     public static void printItems(String[] items) {
         // TODO: Implement this method here!
+        for(String item: items){
+            System.out.println(item);
+        }
     }
 
     /**

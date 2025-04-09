6f2d566aa837b5cbce0c5e47a456df6df3f8bcb2
Iquer_Dough <206500499+DennysSaenz123@users.noreply.github.com>
2025-04-09 09:09:29 -0700

    Solved third problem, used for each loop and checked if array was empty or if the words did not start with a

--- src/Practice.java
+++ src/Practice.java
@@ -77,7 +77,13 @@ public class Practice {
      */
     public static boolean allStartWithA(String[] words) {
         // TODO: Delete the dummy return statement and implement this method here!
-        return false;
+        for(word: words){
+            if(words.length == 0 || !word.toLowerCase().startsWith("a")){
+                return false
+            } 
+        } 
+        return true ;
+        }
     }
 
     public static void main(String[] args) {
